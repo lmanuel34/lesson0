46aa89a3e2dea16b9aac62c2a238f1646940f3bd
landon <lmanuel@uco.edu>
2021-01-19 21:24:59 -0600

    cloud function call

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,13 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.doSomething = functions.https.onCall(doSomething)
+
+function doSomething(data, context){
+    const n1 = data.n1
+    const n2 = data.n2
+
+    return {
+        add: n1 + n2,
+        mul: n1 * n2
+    }
+}
--- public/app.js
+++ public/app.js
@@ -1,6 +1,19 @@
+const f1 = firebase.functions().httpsCallable('doSomething')
+
 const buttonTag = document.getElementById('button-lucky')
 buttonTag.addEventListener('click', buttonClick)
 
-function buttonClick() {
+async function buttonClick() {
     console.log('button clicked')
+    try{
+        const n1 = Math.floor(Math.random() * 100) 
+        const n2 = Math.floor(Math.random() * 100) 
+        const result = await f1({n1, n2})
+        const ret = result.data
+        console.log(ret)
+        const message = document.getElementById('message')
+        message.innerHTML = 'n1 = ' + n1 + ', n2 = ' + n2 + '<br>' + 'add: ' + ret.add + ' mul: ' + ret.mul 
+    }catch(e){
+        console.log(e)
+    }
 }
